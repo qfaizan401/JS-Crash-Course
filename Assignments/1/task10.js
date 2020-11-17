456d359982e4b0e30d24fe01afdc9dcd357a3b5d
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-11-17 16:38:37 +0500

    task10

---
+++ Assignments/1/task10.js
@@ -0,0 +1,8 @@
+let num;
+document.write(`Value after variable declaration is: ${num}<br>`)
+num = 5
+document.write(`Initial value: ${num}<br>
+Value after increment is: ${num+=1}<br>
+Value after addition is: ${num+=7}<br>
+Value after decrement is: ${num-=1}<br>
+The remainder is: ${num%=3}`)
