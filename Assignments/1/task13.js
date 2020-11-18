ffe2c154e834e7ed69a325829bab620d9eeec3a2
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-11-18 23:38:33 +0500

    task13

---
+++ Assignments/1/task13.js
@@ -0,0 +1,12 @@
+let inpuVal = parseInt(prompt("Enter any number"))
+document.write(`Result:<br>
+The value of a in ${inpuVal}<br>
+....................................<br>
+The value of ++a is ${++inpuVal}<br>
+Now the value of a is ${inpuVal}<br>
+The value of a++ is ${inpuVal++}<br>
+Now the value of a is ${inpuVal}<br>
+The value of --a is ${--inpuVal}<br>
+Now the value of a is ${inpuVal}<br>
+The value of a-- is ${inpuVal--}<br>
+Now the value of a is ${inpuVal}<br>`)
