85847a1dead6412c13d8f7a9e559a5492b918556
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-11-19 00:48:52 +0500

    task2

---
+++ Assignments/2/task2.js
@@ -0,0 +1,9 @@
+let inputColor = prompt("Enter Color").toLowerCase()
+
+if (inputColor == 'red') {
+    document.write('Must Stop')
+} else if (inputColor == 'yellow') {
+    document.write('Ready to move')
+} else if (inputColor == 'green') {
+    document.write('Move now')
+}
