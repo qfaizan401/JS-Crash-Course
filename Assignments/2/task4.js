bf52ad2c41e632247c54a2ebb91fb5e5c1a4a6da
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-11-19 09:52:47 +0500

    task4

--- Assignments/2/task4.js
+++ Assignments/2/task4.js
@@ -8,9 +8,10 @@ inputData = () => {
 }
 
 computation = (sub1,sub2,sub3,totalMarks) => {
-    let displayData = [totalMarks]; obtainMarks = sub1+sub2+sub3
+    let displayData = [totalMarks]; 
+    let obtainMarks = sub1+sub2+sub3
     displayData.push(obtainMarks)
-    let percent = (obtainMarks/totalMarks)*100
+    let percent = Number(((obtainMarks/totalMarks)*100).toFixed(2))
     displayData.push(percent)
     return displayData
 }
@@ -19,13 +20,28 @@ decisionMaking = (percent) => {
     let grade, remarks
     if (percent > 80) {
         grade = 'A-one'; remarks = 'Excellent'
-    } else if (80<percent<70){
+    } else if (80>percent>70){
         grade = 'A'; remarks = 'Good'
-    } else if (70<percent<60){
+    } else if (70>percent>60){
         grade = 'B'; remarks = 'You need to improve'
-    } else {
+    } else if (percent<60) {
         grade = 'F'; remarks = 'Sorry'
     }
     return [grade, remarks]
 }
 
+const userInput = inputData()
+let [sub1, sub2, sub3, totalMarks] = userInput
+
+const processData = computation(sub1, sub2, sub3, totalMarks)
+let [totalMark, obtainMarks, percent] = processData
+
+const grading = decisionMaking(percent)
+let [grade, remarks] = grading
+
+document.getElementById('lvl1').innerHTML = `Marks Sheet`
+document.getElementById('content').innerHTML = `Total Marks: ${totalMarks} <br>
+Marks Obtained: ${obtainMarks} <br>
+Percentage: ${percent}% <br>
+Grade: ${grade} <br>
+Remarks: ${remarks}`
