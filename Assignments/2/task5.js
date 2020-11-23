a05955901be7d41ab6a06f3d124453dd4dbff67e
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-11-24 01:25:33 +0500

    Update task5.js

--- Assignments/2/task5.js
+++ Assignments/2/task5.js
@@ -1,10 +1,10 @@
 let num = Math.floor(Math.random()*10)
 let userInput = parseInt(prompt('Enter a number'))
 
-if (userInput == 7) {
-    document.write('Bingo! Correct answer')
+if (userInput == num) {
+    document.write(`Bingo! Correct answer`)
 } else if (userInput == num+1 || userInput ==  num-1) {
-    document.write('Close enough to the correct answer')
+    document.write(`Close enough to the correct answer <br> Guess number is ${num}`)
 } else {
-    document.write('Too Far')
+    document.write(`Too Far <br> Guess number is ${num}`)
 }
