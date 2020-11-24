a1f7035ebf78f7b174540c5957c81dfd90eb005f
Muhammad Faizan Qureshi <qfaizan401@gmail.com>
2020-11-24 15:44:41 +0500

    task8

---
+++ Assignments/2/task8.js
@@ -0,0 +1,8 @@
+let num = parseInt(prompt(`Enter a number`))
+if (num < 1) {
+    document.write(`Negative`)
+} else if (num > 1) {
+    document.write(`Positive`)
+} else{
+    document.write(`Zero`)
+}
